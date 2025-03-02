9c68a413c2a61407cc02b1b023e3e9b7a030c1f9
abdullahankon <abdullahb.faruk@gmail.com>
2025-03-02 16:59:46 +0600

    Fixed all sections and final version

--- scripts/navigate.js
+++ scripts/navigate.js
@@ -4,8 +4,3 @@ blogs.addEventListener("click", () => {
   window.location.href = "blog.html";
 });
 
-const backToDeskButton = document.getElementById("back-to-desk");
-
-backToDeskButton.addEventListener("click", function () {
-  window.location.href = "index.html";
-});
